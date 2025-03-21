86332d57c0955a7614c8aa9e3c3f4baf6f6bde0a
Pallandos <jean.caylus@ensta.fr>
2025-03-21 13:29:28 +0100

    go upd

--- go/main.go
+++ go/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello Go!!")
+	file_path := "../source/src.txt"
+
+	// ouverture
+	file, err := os.Open(file_path)
+	if err != nil {
+		log.Fatalf("Erreur d'ouverture : %v", err)
+	}
+	defer file.Close() // fermeture
+
+	// lecture
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	// gestion des erreurs de lecture
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Erreur de lecture : %v", err)
+	}
 }
