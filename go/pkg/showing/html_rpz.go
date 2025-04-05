49f215748461745c8feeef1a1471afd4a101d16b
jeremlechat <lacuisine@tutanota.com>
2025-04-05 18:58:35 +0200

    modif samedi

--- go/pkg/showing/html_rpz.go
+++ go/pkg/showing/html_rpz.go
@@ -1,8 +1,11 @@
-package main
+package html_rpz
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/Pallandos/Compidown/pkg/lexemes"
 )
 
 func Rpz_title(line string, level int) {
@@ -58,6 +61,98 @@ func Rpz_fencedcode(line string, language string) {
 	}
 }
 
+func Rpz_intentedcode(line string) {
+	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()                                                  // on ferme automatiquement à la fin de notre programme
+	_, err = file.WriteString("<pre><code>" + line + "</code></pre>\n") // écrire dans le fichier
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Rpz_quote(line string) {
+	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()                                                   // on ferme automatiquement à la fin de notre programme
+	_, err = file.WriteString("<blockquote>" + line + "</blockquote>\n") // écrire dans le fichier
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Rpz_orderedlist(line string) {
+	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()                         // on ferme automatiquement à la fin de notre programme
+	_, err = file.WriteString(line + "<br>\n") // écrire dans le fichier
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Rpz_bulletlist(line string) {
+	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()                                   // on ferme automatiquement à la fin de notre programme
+	_, err = file.WriteString("<li>" + line + "</li>\n") // écrire dans le fichier
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Rpz_paragraph(line string) {
+	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()                                 // on ferme automatiquement à la fin de notre programme
+	_, err = file.WriteString("<p>" + line + "</p>\n") // écrire dans le fichier
+	if err != nil {
+		panic(err)
+	}
+}
+
+func showblock(line lexemes.Inline) {
+	// on affiche le bloc
+	fmt.Println("Genre : " + line.Genre)
+	fmt.Println("Texte : " + line.Text)
+	switch line.Genre {
+	case "Title":
+		// Rpz_title(line.Text, line.caracts.TitleLevel)
+	case "BlankLine":
+		Rpz_blankline()
+	case "ThemeBreak":
+		Rpz_themebreak()
+	case "FencedCode":
+		//Rpz_fencedcode(line.Text, line.Caracts.Language)
+	case "IndentCode":
+		Rpz_intentedcode(line.Text)
+	case "Quote":
+		Rpz_quote(line.Text)
+	case "OrderedList":
+		Rpz_orderedlist(line.Text)
+	case "BulletList":
+		Rpz_bulletlist(line.Text)
+	case "Paragraph":
+		Rpz_paragraph(line.Text)
+	}
+}
+
+func Showtext(lines []lexemes.Inline) {
+	for _, inline := range lines {
+		showblock(inline)
+	}
+}
+
 func Import_style() {
 	file, err := os.OpenFile("test.html", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err != nil {
@@ -68,13 +163,4 @@ func Import_style() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func main() {
-	Import_style()
-	Rpz_title("Carotte", 2)
-	Rpz_blankline()
-	Rpz_title("Patate", 6)
-	Rpz_themebreak()
-	Rpz_fencedcode("func main() {", "go")
 }
